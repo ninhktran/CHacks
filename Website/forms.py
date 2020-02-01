d02de9b03168c7384c92918aee1e8fc34c83bc78
Anthony Asp <46567012+AAspCodes@users.noreply.github.com>
2020-02-01 14:14:26 -0800

    add unique email validation

--- Website/forms.py
+++ Website/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, ValidationError
+from Website.models import Subscriber
 
 class RegistrationFrom(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
@@ -9,3 +10,9 @@ class RegistrationFrom(FlaskForm):
     weather = BooleanField("Weather")
     sports = BooleanField("Sports")
     submit = SubmitField('Submit')
+
+    def validate_email(self, email):
+        user = Subscriber.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError(
+                'That email is taken. Please choose another one.')
