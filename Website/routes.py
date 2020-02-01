50bfe56cbe477d3eeb5a0260a439a5a37765ffca
Anthony Asp <46567012+AAspCodes@users.noreply.github.com>
2020-02-01 13:55:06 -0800

    implent Subscriber entry into the db

--- Website/routes.py
+++ Website/routes.py
@@ -1,8 +1,26 @@
-from flask import render_template, url_for
-from Website import app
+from flask import render_template, url_for, flash, redirect
+from Website import app, db
 from Website.forms import RegistrationFrom
+from Website.models import Subscriber
 
 @app.route("/", methods=['GET','POST'])
 def home():
     form = RegistrationFrom()
+    if form.validate_on_submit():
+        print('form valid')
+        subscriber = Subscriber(email=form.email.data,
+                                everett=form.everett.data,
+                                skagit_county=form.skagit_county.data,
+                                weather=form.weather.data,
+                                sports=form.sports.data)
+        db.session.add(subscriber)
+        db.session.commit()
+        flash(f'Your account has been created! You are now able to log in.',
+              'success')
+        return redirect(url_for('home'))
+
+    else:
+        print('form not valid')
+
+
     return render_template("home.html", form=form)
