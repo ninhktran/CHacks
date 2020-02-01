fa9df8fc9c65ddc688d723db42110cb97e635ed6
Anthony Asp <46567012+AAspCodes@users.noreply.github.com>
2020-02-01 15:06:17 -0800

    implement valid agency email function

--- Website/dbQuery.py
+++ Website/dbQuery.py
@@ -1,5 +1,10 @@
-from Website import db
+from Website.models import Agency, Subscriber
 
 def check_valid_email(email):
+    agency = Agency.query.filter_by(email=email).first()
+    if agency:
+        return True
+    else:
+        return False
 
     pass
