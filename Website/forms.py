887204e2874cc4069beea00b68cc197295be5646
Anthony Asp <46567012+AAspCodes@users.noreply.github.com>
2020-02-01 12:26:38 -0800

    add email label

--- Website/forms.py
+++ Website/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Email
 
 class RegistrationFrom(FlaskForm):
-    email = StringField(validators=[DataRequired(), Email()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     everett = BooleanField("Everett")
     skagit_county = BooleanField("Skagit County")
     weather = BooleanField("Weather")
