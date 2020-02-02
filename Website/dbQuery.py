66fabdc383897b21faece1b58d135fd2eab451fc
Anthony Asp <46567012+AAspCodes@users.noreply.github.com>
2020-02-01 16:18:44 -0800

    Update dbQuery.py

--- Website/dbQuery.py
+++ Website/dbQuery.py
@@ -7,8 +7,28 @@ def check_valid_email(email):
     else:
         return False
 
-def get_interested_subs(areas,topics):
-    pass
+def get_interested_subs(area,topic):
+    subscribers = Subscriber.query.all()
+    print(subscribers)
+    matched_adds = []
+    for sub in subscribers:
+        topic_match = False
+        area_match = False
+        for a in area:
+            if a in sub.area.split(','):
+                # print('area match')
+                area_match = True
+        for t in topic:
+            if t in sub.topic.split(','):
+                # print('topic match')
+                topic_match = True
+        if area_match and topic_match:
+            matched_adds.append(sub.email)
+
+
+
+    print(matched_adds)
+    return matched_adds
 
 # print(check_valid_email("bigbrother@gmail.com"))
 get_interested_subs(['everett'],['weather'])
