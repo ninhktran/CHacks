6ffdc25c47b51f36665518ce4ada5215700fd9f7
Anthony Asp <46567012+AAspCodes@users.noreply.github.com>
2020-02-01 16:19:50 -0800

    change subscriber model for easier db query

--- Website/models.py
+++ Website/models.py
@@ -2,14 +2,23 @@ from Website import db
 
 class Subscriber(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+
     email = db.Column(db.String(120), unique=True, nullable=False)
-    everett = db.Column(db.Boolean(), default=False)
-    skagit_county = db.Column(db.Boolean(), default=False)
-    weather = db.Column(db.Boolean(), default=False)
-    sports = db.Column(db.Boolean(), default=False)
+    area = db.Column(db.String(120),nullable=False)
+    topic = db.Column(db.String(120), nullable=False)
+    # everett = db.Column(db.Boolean(), default=False)
+    # skagit_county = db.Column(db.Boolean(), default=False)
+    # weather = db.Column(db.Boolean(), default=False)
+    # sports = db.Column(db.Boolean(), default=False)
 
     def __repr__(self):
-        return f"User('{self.email}', '{self.everett}', '{self.skagit_county}', '{self.weather} '{self.sports},)"
+        return f"User('{self.email}', '{self.area}', '{self.topic}')"
+
+    def get_area(self):
+        return self.area
+
+    def get_topic(self):
+        return self.topic
 
 
 class Agency(db.Model):
