ae2e184849a71fda660877ff1c340d4ab119976c
Anthony Asp <46567012+AAspCodes@users.noreply.github.com>
2020-02-01 22:40:29 -0800

    Update find_sub.py

--- Website/find_sub.py
+++ Website/find_sub.py
@@ -1,19 +1,10 @@
-def get_msg():
-    with open('testing', 'r') as f:
-        contents = str(f.read())
-    return contents
+from email.parser import BytesParser
+from email.policy import default
 
-def find_sub():
-    msg = get_msg()
-    thingy = msg.find("Subject: Millions of dollars in Earned Income Tax Credits go unclaimed each")
-    start_pos = msg.find("\nSubject:")
-    end_pos = msg.find("\nTo:")
-
-    print(start_pos,end_pos)
-    return msg[thingy: thingy+150]
-
-
-
-
-
-print(find_sub())
+def get_meta_data():
+    with open('bytes_message', 'rb') as fp:
+        headers = BytesParser(policy=default).parse(fp)
+    to = headers['to']
+    frm = headers['from']
+    subject = headers['subject']
+    return to, frm, subject
