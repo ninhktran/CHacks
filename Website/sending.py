390846d83df449d9bc373a3e44925c056d3b352f
Anthony Asp <46567012+AAspCodes@users.noreply.github.com>
2020-02-01 17:40:59 -0800

    slim sending.py

--- Website/sending.py
+++ Website/sending.py
@@ -1,44 +1,35 @@
 import smtplib
-
-from string import Template
-
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-
 
 MY_ADDRESS = 'alphatester721@gmail.com'
 PASSWORD = '@Mamamia2'
 
-
 def get_contacts(filename):
     """
-    Return two lists names, emails containing names and email addresses
+    Return one list of email addresses
     read from a file specified by filename.
     """
-
-    names = []
     emails = []
     with open(filename, mode='r', encoding='utf-8') as contacts_file:
-        for a_contact in contacts_file:
-            names.append(a_contact.split()[0])
-            emails.append(a_contact.split()[1])
-    return names, emails
+        for a_contact in contacts_file.read().split('\n'):
+            emails.append(a_contact)
+    return emails
 
 
-def read_template(filename):
+def read_message(filename):
     """
-    Returns a Template object comprising the contents of the
-    file specified by filename.
+    Returns a String a of the message
     """
 
-    with open(filename, 'r', encoding='utf-8') as template_file:
-        template_file_content = template_file.read()
-    return Template(template_file_content)
+    with open(filename, 'r') as f:
+        template_file_content = f.read()
+    return template_file_content
 
 
 def main():
-    names, emails = get_contacts('mycontacts2')  # read contacts
-    message_template = read_template('message')
+    emails = get_contacts('mycontacts2')  # read contacts
+    message = read_message('message.html')
 
     # set up the SMTP server
     s = smtplib.SMTP(host='smtp.gmail.com', port=587)
@@ -47,25 +38,22 @@ def main():
     s.login(MY_ADDRESS, PASSWORD)
 
     # For each contact, send the email:
-    for name, email in zip(names, emails):
-        msg = MIMEMultipart()  # create a message
+    for email in emails:
+        print(email)
+        msg = MIMEMultipart()  # create a message.html
 
-        # add in the actual person name to the message template
-        message = message_template.
-        # message = message_template.substitute(PERSON_NAME=name.title())
+        # add in the actual person name to the message.html template
 
-        # Prints out the message body for our sake
-        # print(message)
 
-        # setup the parameters of the message
+        # setup the parameters of the message.html
         msg['From'] = MY_ADDRESS
         msg['To'] = email
         msg['Subject'] = "This is TEST from your local Government Agency"
+        # msg.attach(message)
+        # add in the message.html body
+        msg.attach(MIMEText(message, 'html'))
 
-        # add in the message body
-        msg.attach(MIMEText(message, 'plain'))
-
-        # send the message via the server set up earlier.
+        # send the message.html via the server set up earlier.
         s.send_message(msg)
         del msg
 
@@ -75,7 +63,7 @@ def main():
 
 if __name__ == '__main__':
     main()
-# Collapse
+
 
 
 
